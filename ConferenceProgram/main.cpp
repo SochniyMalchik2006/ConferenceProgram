86c3cc6344e26aa262d4800051a4d597bb1d9710
Roman Tikhonov <romatikhono06mail@.ru>
2023-12-20 14:56:21 +0300

    Добавлены тестовые данные

--- ConferenceProgram/main.cpp
+++ ConferenceProgram/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #8. GIT\n";
     cout << "Variant #2. Conference Program\n";
     cout << "Author: Roman Tikhonov\n";
+    cout << "Group: 21PINZH1D\n";
     return 0;
 }
